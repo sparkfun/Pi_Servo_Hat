fd614103e11c056f928bfa3715967b76f5b3cb6d
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2019-09-10 21:43:51 -0600

    Update servohat_50Hz.py

--- Examples/servohat_50Hz.py
+++ Examples/servohat_50Hz.py
@@ -4,12 +4,12 @@ addr = 0x40
 
 ## Running this program will move the servo to 0, 45, and 90 degrees with 5 second pauses in between with a 50 Hz PWM signal.
 
-bus.write_byte_data(addr, 0, 0x20) # enable the chip
+bus.write_byte_data(addr, 0, 0x20) # enables word writes
 time.sleep(.25)
 bus.write_byte_data(addr, 0, 0x10) # enable Prescale change as noted in the datasheet
 time.sleep(.25) # delay for reset
 bus.write_byte_data(addr, 0xfe, 0x79) #changes the Prescale register value for 50 Hz, using the equation in the datasheet.
-bus.write_byte_data(addr, 0, 0x20) # enables the chip
+bus.write_byte_data(addr, 0, 0x20) # enables word writes
 
 time.sleep(.25)
 bus.write_word_data(addr, 0x06, 0) # chl 0 start time = 0us
